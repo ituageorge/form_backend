7dc5992349c3ae0c02febde0de73b01a3d489fef
ituageorge <50578363+ituageorge@users.noreply.github.com>
2021-10-01 22:45:12 +0100

    a third commiting

--- server.js
+++ server.js
@@ -56,13 +56,13 @@ app.use(
 
 app.use('/users', routes);
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
     app.use(express.static('dist/'));
 
     app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
-}
+// }
 
 app.listen( PORT , () => {
   console.log('Server listening on port ' + PORT);
