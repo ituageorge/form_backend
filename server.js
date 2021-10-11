452e7de1f259172a61ffaebc71a975357c45a8a8
ituageorge <50578363+ituageorge@users.noreply.github.com>
2021-10-11 17:18:43 +0100

    removed dist folder from the gitignore folder

--- server.js
+++ server.js
@@ -56,13 +56,21 @@ app.use(
 
 app.use('/users', routes);
 
-if(process.env.NODE_ENV === 'production') {
-  // set static folder
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'form_backend', 'dist', 'index.html'));
+});
+
+
+// if(process.env.NODE_ENV === 'production') {
+//   // set static folder
+//   app.use(express.static('client/build'));
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
 
 app.listen( PORT , () => {
   console.log('Server listening on port ' + PORT);
